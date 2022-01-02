968af4b12fedccca6acc9abf7fdca895ab0b6025
Alex.Ramirez <aleramirez201@gmail.com>
2022-01-01 20:31:44 -0600

    added the enter alert that asks for how many charaters

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,11 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 let loweralphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-let upperAlphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase.split("");
+let upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 let specialCharaters = "`~!@#$%^&*,./?=+".split("")
 let numbers = "0123456789".split("")
 let selectedArrays = [];
+let randomArray = [];
 
 // Write password to the #password input
 function writePassword() {
@@ -18,17 +19,25 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword() {
+  enter = parseInt(prompt("Choose btween 8 and 128 charaters for your password"));
+  if (!enter) {
+    alert("Please enter a value");
+  } else if (enter < 8 || enter > 128){
+    enter = parseInt(prompt("Please enter a value between 8 and 128"));
+  }
+}
 
-let randomNumber = Math.floor(Math.random()*upperAlphabet.length);
+// let randomNumber = Math.floor(Math.random()*upperAlphabet.length);
 
-upperAlphabet[randomNumber];
+// upperAlphabet[randomNumber];
 
-let password = "";
-for(let i=0; i<100; 1++){
-  password+="a";
-};
+// let password = "";
+// for(let i=0; i<100; 1++){
+//   password+="a";
+// };
 
-if (placeHolder){
-  selectedArrays = [...loweralphabet, ...upperAlphabet];
+// if (placeHolder){
+//   selectedArrays = [...loweralphabet, ...upperAlphabet];
 
-};
+// };
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 1) added Develop file from the hw so it could be here
 2) added things from the zoom recording as a base start
 3) added more let variables for the password generator
+4) added the first promt that allows the user to enter the charaters
+5)
